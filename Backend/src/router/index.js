d2b94b2ef871fd18b578a6f26a50b65f2d8cb9ee
belgacem-dahmen <belgacem.dahmen@gmail.com>
2023-12-22 18:52:10 +0100

    adding 404 Page

--- Backend/src/router/index.js
+++ Backend/src/router/index.js
@@ -1,96 +1,100 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AuthLayout from "../layouts/AuthLayout.vue"
-import DefaultLayout from '../layouts/DefaultLayout.vue'
-import Dashboard from '../views/Dashboard.vue'
-import Login from '../views/Auth/Login.vue'
-import Register from '../views/Auth/Register.vue'
-import ForgotPassword from '../views/Auth/ForgotPassword.vue'
-import Adherents from '../views/Adherents.vue'
-import Demandes from '../views/Demandes.vue'
-import Formations from '../views/Formations.vue'
-import Exports from '../views/Exports.vue'
-import { useUserStore } from '../stores/userStore'
+import { createRouter, createWebHistory } from "vue-router";
+import AuthLayout from "../layouts/AuthLayout.vue";
+import DefaultLayout from "../layouts/DefaultLayout.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Auth/Login.vue";
+import Register from "../views/Auth/Register.vue";
+import ForgotPassword from "../views/Auth/ForgotPassword.vue";
+import Adherents from "../views/Adherents.vue";
+import Demandes from "../views/Demandes.vue";
+import Formations from "../views/Formations.vue";
+import Exports from "../views/Exports.vue";
+import notFound from "../views/notFound.vue";
+import { useUserStore } from "../stores/userStore";
 
-
-
-    const routes = [
-        {path:'/',
-        name:"home",
-        component:AuthLayout,
-        meta: {requiresAuth : true},
-        children :   [
-            {
-            path :'/',
-            name: 'Dashboard',
-            component : Dashboard
-             },
-             {
-                path :'/adherents',
-               name: 'Adherents',
-               component : Adherents
-            },
-            {
-                path :'/demandes',
-                name: 'Demandes',
-                component : Demandes
-                 },
-                 {
-                    path :'/formations',
-                    name: 'Formations',
-                    component : Formations
-                     },{
-                        path :'/exports',
-                        name: 'Exports',
-                        component : Exports
-                         }
-        
-        
-        
-        
-        ]},
-        {
-          path:'/auth',
-          redirect:'/login',
-          name:'Auth',
-          component: DefaultLayout,
-          children : [ {
-            path:'/login',
-            name:'Login',
-            component:Login
-        },
-        {
-            path:'/register',
-            name:'Register',
-            component:Register
-        },
-        {
-            path:'/forgot-password',
-            name:'ForgotPassword',
-            component:ForgotPassword
-        }]
-        },
-    
-       ]
-
- const router = createRouter({
-    history: createWebHistory(),
-    routes 
-})
-
-router.beforeEach((to, from,next) => {
-    const user = useUserStore()
-    // instead of having to check every route record with
-    // to.matched.some(record => record.meta.requiresAuth)
-    if (to.meta.requiresAuth && !user.isLoggedIn ) {
-        next({name :'Login'})
-    }
-      else if (user.isLoggedIn && to.name=== 'Login' || to.name=== 'Register'  ) {
-        next({name : 'Dashboard'})
-      } else
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: AuthLayout,
+    meta: { requiresAuth: true },
+    children: [
       {
-        next()
-      }
-    }
-  )
+        path: "/",
+        name: "Dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/adherents",
+        name: "Adherents",
+        component: Adherents,
+      },
+      {
+        path: "/demandes",
+        name: "Demandes",
+        component: Demandes,
+      },
+      {
+        path: "/formations",
+        name: "Formations",
+        component: Formations,
+      },
+      {
+        path: "/exports",
+        name: "Exports",
+        component: Exports,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register,
+      },
+      {
+        path: "/forgot-password",
+        name: "ForgotPassword",
+        component: ForgotPassword,
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: notFound,
+  },
+];
 
-export default router
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const user = useUserStore();
+  // instead of having to check every route record with
+  // to.matched.some(record => record.meta.requiresAuth)
+  if (to.meta.requiresAuth && !user.isLoggedIn) {
+    next({ name: "Login" });
+  } else if (
+    (user.isLoggedIn && to.name === "Login") ||
+    to.name === "Register"
+  ) {
+    next({ name: "Dashboard" });
+  } else {
+    next();
+  }
+});
+
+export default router;
---
+++ Backend/src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full h-screen flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center justify-center">
+      <p class="text-3xl md:text-4xl lg:text-5xl text-gray-800 mt-12">
+        Page Not Found
+      </p>
+      <p class="md:text-lg lg:text-xl text-gray-600 mt-8">
+        Sorry, the page you are looking for could not be found.
+      </p>
+    </div>
+    <div>
+      <button
+        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 mt-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        <router-link :to="{ name: 'Dashboard' }">Go back Home </router-link>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
